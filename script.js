f8af0c7d93e0f7f60585f8df8be1987e5aea8700
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2024-01-26 19:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Create an object called student
+const student = {
+  name: 'John',
+};
+
+// Add a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+// Example usage
+const keys = student.getKeys();
+console.log(keys); // Output: ['name']
